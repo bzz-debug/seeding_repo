c99499601de6d1fc9adff32d26df05b0d731118f
bzz-debug <ben.sumner@hotmail.com>
2025-05-02 11:25:46 +0100

    adding further alteration on listener file

--- listen.js
+++ listen.js
@@ -1,7 +1,7 @@
 const app = require("./app.js");
 const { PORT = 75 } = process.env;
 
-app.listen(75, (err) => {
+app.listen(PORT, (err) => {
   if (err) console.log("port not open");
   console.log("server listening on port 75");
 });
