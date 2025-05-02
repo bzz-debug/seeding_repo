e490b47eb75ca727f4e542a91010e1ec0f6c2563
bzz-debug <ben.sumner@hotmail.com>
2025-05-02 11:06:53 +0100

    adding taks 1-6 from hosting exercise

--- listener.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.listen(75, , function(err){
-    if (err) console.log("port not open")
-    console.log("server listening on port 75");
-});
